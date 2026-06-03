4d5003d60a9c7a476581cbf530b1f8f1088576fc
Almatulus <a.kaipzhanov@gmail.com>
2026-06-03 19:31:49 +0500

    feat(ui): добавил header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
+<script setup>
+import { ClockIcon, CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+
 <template>
-  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
-    <div class="bg-white p-8 rounded-lg shadow-md">
-      <h1 class="text-3xl font-bold text-blue-600 mb-4">🎉 Tailwind v4 работает!</h1>
-      <p class="text-gray-600 mb-4">Это современный подход без конфигурации</p>
-      <button class="text-white bg-blue-500 px-4 py-2 rounded hover:bg-blue-600 transition-colors">
-        Кнопка со стилями
-      </button>
-    </div>
-  </div>
+  <header
+    class="sticky top-0 z-50 flex items-center justify-between p-2 border-b border-gray-400 bg-white"
+  >
+    <a href="#" class="flex items-center">
+      <ClockIcon class="w-10" />
+      <p class="font-semibold">MyTracker</p>
+    </a>
+    <a href="#" class="text-sm">
+      <div v-if="true" class="flex items-center gap-1 font-medium">
+        Day complete!
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+
+      <div v-else class="flex items-center gap-1 font-medium">
+        Progress: 20%
+        <div class="bg-red-500 h-3 w-3 rounded-full"></div>
+      </div>
+    </a>
+  </header>
+  <main>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugiat culpa blanditiis
+    repellat quo placeat ipsum iste incidunt aliquam, iusto sint nobis ex fuga quae architecto
+    laboriosam earum necessitatibus rem! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Molestiae fugiat culpa blanditiis repellat quo placeat ipsum iste incidunt aliquam, iusto sint
+    nobis ex fuga quae architecto laboriosam earum necessitatibus rem! Lorem, ipsum dolor sit amet
+    consectetur adipisicing elit. Molestiae fugiat culpa blanditiis repellat quo placeat ipsum iste
+    incidunt aliquam, iusto sint nobis ex fuga quae architecto laboriosam earum necessitatibus rem!
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugiat culpa blanditiis
+    repellat quo placeat ipsum iste incidunt aliquam, iusto sint nobis ex fuga quae architecto
+    laboriosam earum necessitatibus rem! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Molestiae fugiat culpa blanditiis repellat quo placeat ipsum iste incidunt aliquam, iusto sint
+    nobis ex fuga quae architecto laboriosam earum necessitatibus rem! Lorem, ipsum dolor sit amet
+    consectetur adipisicing elit. Molestiae fugiat culpa blanditiis repellat quo placeat ipsum iste
+    incidunt aliquam, iusto sint nobis ex fuga quae architecto laboriosam earum necessitatibus rem!
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugiat culpa blanditiis
+    repellat quo placeat ipsum iste incidunt aliquam, iusto sint nobis ex fuga quae architecto
+    laboriosam earum necessitatibus rem! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Molestiae fugiat culpa blanditiis repellat quo placeat ipsum iste incidunt aliquam, iusto sint
+    nobis ex fuga quae architecto laboriosam earum necessitatibus rem! Lorem, ipsum dolor sit amet
+    consectetur adipisicing elit. Molestiae fugiat culpa blanditiis repellat quo placeat ipsum iste
+    incidunt aliquam, iusto sint nobis ex fuga quae architecto laboriosam earum necessitatibus rem!
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugiat culpa blanditiis
+    repellat quo placeat ipsum iste incidunt aliquam, iusto sint nobis ex fuga quae architecto
+    laboriosam earum necessitatibus rem!
+  </main>
 </template>
