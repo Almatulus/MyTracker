1e17438d207109278edfdeb6f93074da4c03df63
Almatulus <a.kaipzhanov@gmail.com>
2026-06-05 21:32:16 +0500

    feat(ui): Верстка страницы TheTimeLine

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,1 +1,18 @@
-<template><div class="">timeline</div></template>
+<template>
+  <div class="mt-10">
+    <ul>
+      <li
+        v-for="i in 6"
+        :key="i"
+        class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4"
+      >
+        <a
+          href="#"
+          class="absolute -top-4 left-1/2 -translate-x-1/2 rounded bg-gray-100 px-2 text-lg font-mono"
+        >
+          {{ i }}:00
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
