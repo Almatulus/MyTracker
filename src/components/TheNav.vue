f59f99d2a35574f8eb286d3c5f0383a752e5fd90
Almatulus <a.kaipzhanov@gmail.com>
2026-06-05 18:58:47 +0500

    refactor: декомпозиция названия страниц в отдельный файл-константу и нормализация хэша страницы

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,15 +1,28 @@
 <script setup>
 import { ref } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/solid'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../pageConstants'
 import NavItem from './NavItem.vue'
 
 const menuList = ref({
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon,
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon,
 })
 
-const currentPage = ref('timeline')
+const currentPage = ref(normalizeHash())
+
+function normalizeHash() {
+  const hash = window.location.hash.slice(1)
+
+  if (Object.keys(menuList).includes(hash)) {
+    return hash
+  }
+
+  window.location.hash = PAGE_TIMELINE
+
+  return PAGE_TIMELINE
+}
 </script>
 
 <template>
