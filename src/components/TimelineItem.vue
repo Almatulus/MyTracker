241d5dde0cf3c72886c6b5a7d6be9f06fe5f1502
Almatulus <a.kaipzhanov@gmail.com>
2026-06-07 20:58:45 +0500

    feat: Создание компонента TimelineItem

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  timelineItem: {
+    type: Object,
+    required: true,
+  },
+})
+
+const linkClasses = [
+  'absolute -top-4 left-1/2 -translate-x-1/2 rounded  px-2  font-mono',
+  props.timelineItem.hour === new Date().getHours()
+    ? 'bg-purple-500 text-white font-bold text-lg'
+    : 'bg-gray-100 text-grey-500',
+]
+</script>
+
+<template>
+  <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
+    <a href="#" :class="linkClasses"> {{ timelineItem.hour }}:00 </a>
+  </li>
+</template>
--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { generateTimeItems } from '@/functions'
+import TimelineItem from '@/components/TimelineItem.vue'
 
 const timeItems = generateTimeItems()
 </script>
@@ -7,18 +8,7 @@ const timeItems = generateTimeItems()
 <template>
   <div class="mt-10">
     <ul>
-      <li
-        v-for="{ hour } in timeItems"
-        :key="hour"
-        class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4"
-      >
-        <a
-          href="#"
-          class="absolute -top-4 left-1/2 -translate-x-1/2 rounded bg-gray-100 px-2 text-lg font-mono"
-        >
-          {{ hour }}:00
-        </a>
-      </li>
+      <TimelineItem v-for="item in timeItems" :key="item" :timeline-item="item" />
     </ul>
   </div>
 </template>
