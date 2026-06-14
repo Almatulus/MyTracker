7767a3ad7d0777c701110a8c5c409169e772c5d4
Almatulus <a.kaipzhanov@gmail.com>
2026-06-14 20:07:49 +0500

    feat(ui): Верстка страницы TheActivities

--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,1 +1,32 @@
-<template><div class="">activities</div></template>
+<script setup>
+import { ref } from 'vue'
+import { TrashIcon } from '@heroicons/vue/24/solid'
+import VButton from '@/components/VButton.vue'
+import VSelect from '@/components/VSelect.vue'
+
+const periodOptions = [
+  { value: 15, label: '0:15' },
+  { value: 30, label: '0:30' },
+  { value: 45, label: '0:45' },
+]
+
+const activities = ['Coding', 'Training', 'Reading']
+</script>
+
+<template>
+  <div class="py-1 px-4">
+    <ul class="divide-y">
+      <li v-for="activity in activities" :key="activity" class="flex flex-col column gap-3 py-4">
+        <div class="flex items-center justify-between">
+          <span class="truncate text-l">{{ activity }}</span>
+          <VButton type="negative" class="p-1">
+            <TrashIcon class="h-6" />
+          </VButton>
+        </div>
+        <div class="">
+          <v-select class="font-mono" placeholder="h:mm" :options="periodOptions"></v-select>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
