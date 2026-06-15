3ec331893a8d3a6f60409689f2f7b9bd1b226110
Almatulus <a.kaipzhanov@gmail.com>
2026-06-15 19:38:48 +0500

    refactor: Декомпозиция элемента активности в отдельный компонент

---
+++ src/components/ActivitiesItem.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+import { TrashIcon } from '@heroicons/vue/24/solid'
+import VButton from '@/components/VButton.vue'
+import VSelect from '@/components/VSelect.vue'
+
+defineProps({
+  activity: {
+    required: true,
+    type: String,
+  },
+})
+
+const periodOptions = [
+  { value: 15, label: '0:15' },
+  { value: 30, label: '0:30' },
+  { value: 45, label: '0:45' },
+]
+
+const secondsToComplete = ref(null)
+</script>
+
+<template>
+  <li class="flex flex-col column gap-3 py-4">
+    <div class="flex items-center justify-between">
+      <span class="truncate text-l">{{ activity }}</span>
+      <VButton type="negative" class="p-1">
+        <TrashIcon class="h-6" />
+      </VButton>
+    </div>
+    <div class="">
+      <VSelect
+        v-model="secondsToComplete"
+        class="font-mono"
+        placeholder="h:mm"
+        :options="periodOptions"
+      ></VSelect>
+    </div>
+  </li>
+</template>
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,14 +1,5 @@
 <script setup>
-import { ref } from 'vue'
-import { TrashIcon } from '@heroicons/vue/24/solid'
-import VButton from '@/components/VButton.vue'
-import VSelect from '@/components/VSelect.vue'
-
-const periodOptions = [
-  { value: 15, label: '0:15' },
-  { value: 30, label: '0:30' },
-  { value: 45, label: '0:45' },
-]
+import ActivitiesItem from '@/components/ActivitiesItem.vue'
 
 const activities = ['Coding', 'Training', 'Reading']
 </script>
@@ -16,17 +7,7 @@ const activities = ['Coding', 'Training', 'Reading']
 <template>
   <div class="py-1 px-4">
     <ul class="divide-y">
-      <li v-for="activity in activities" :key="activity" class="flex flex-col column gap-3 py-4">
-        <div class="flex items-center justify-between">
-          <span class="truncate text-l">{{ activity }}</span>
-          <VButton type="negative" class="p-1">
-            <TrashIcon class="h-6" />
-          </VButton>
-        </div>
-        <div class="">
-          <v-select class="font-mono" placeholder="h:mm" :options="periodOptions"></v-select>
-        </div>
-      </li>
+      <ActivitiesItem v-for="activity in activities" :key="activity" :activity="activity" />
     </ul>
   </div>
 </template>
