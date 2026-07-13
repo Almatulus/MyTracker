5a876ae06d26656ba777a218587dce50efce8742
Almatulus <a.kaipzhanov@gmail.com>
2026-07-13 19:03:11 +0500

    feature: Создание нового компонента VIcon

---
+++ src/components/VIcon.vue
@@ -0,0 +1,41 @@
+<script setup>
+import {
+  ClockIcon as Clock,
+  ListBulletIcon as ListBullet,
+  ChartBarIcon as ChartBar,
+  TrashIcon as Trash,
+  FaceFrownIcon as FaceFrown,
+  PlusIcon as Plus,
+  CheckCircleIcon as CheckCircle,
+  ArrowPathIcon as ArrowPath,
+  PauseIcon as Pause,
+  PlayIcon as Play,
+  XMarkIcon as XMark,
+} from '@heroicons/vue/24/solid'
+
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+})
+
+const icons = {
+  Clock,
+  ListBullet,
+  ChartBar,
+  Trash,
+  FaceFrown,
+  Plus,
+  Clock,
+  CheckCircle,
+  ArrowPath,
+  Pause,
+  Play,
+  XMark,
+}
+</script>
+
+<template>
+  <component :is="icons[name]"></component>
+</template>
