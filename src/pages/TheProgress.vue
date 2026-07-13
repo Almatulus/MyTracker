71575d8bbe7f3260e2ced38a17b21ee296b58d62
Almatulus <a.kaipzhanov@gmail.com>
2026-07-13 20:51:12 +0500

    feature(ui): Верстка страницы с прогрессом TheProgress

--- src/pages/TheProgress.vue
+++ src/pages/TheProgress.vue
@@ -1,1 +1,44 @@
-<template><div class="">progress</div></template>
+<template>
+  <ul class="divide-y">
+    <li class="flex flex-col gap-1 p-4">
+      <div class="truncate text-lg">Activity 1</div>
+      <div class="bg-neutral-200 h-5 overflow-hidden rounded">
+        <div class="bg-red-500 h-full" style="width: 20%"></div>
+      </div>
+      <div class="flex justify-between font-mono">
+        <span>10%</span>
+        <span>03:00 / 30:00</span>
+      </div>
+    </li>
+    <li class="flex flex-col gap-1 p-4">
+      <div class="truncate text-lg">Activity 1</div>
+      <div class="bg-neutral-200 h-5 overflow-hidden rounded">
+        <div class="bg-red-500 h-full" style="width: 20%"></div>
+      </div>
+      <div class="flex justify-between font-mono">
+        <span>10%</span>
+        <span>03:00 / 30:00</span>
+      </div>
+    </li>
+    <li class="flex flex-col gap-1 p-4">
+      <div class="truncate text-lg">Activity 1</div>
+      <div class="bg-neutral-200 h-5 overflow-hidden rounded">
+        <div class="bg-red-500 h-full" style="width: 20%"></div>
+      </div>
+      <div class="flex justify-between font-mono">
+        <span>10%</span>
+        <span>03:00 / 30:00</span>
+      </div>
+    </li>
+    <li class="flex flex-col gap-1 p-4">
+      <div class="truncate text-lg">Activity 1</div>
+      <div class="bg-neutral-200 h-5 overflow-hidden rounded">
+        <div class="bg-red-500 h-full" style="width: 20%"></div>
+      </div>
+      <div class="flex justify-between font-mono">
+        <span>10%</span>
+        <span>03:00 / 30:00</span>
+      </div>
+    </li>
+  </ul>
+</template>
