b53bda14bc967feff8aa9fa9de2ce05f747e4036
Almatulus <a.kaipzhanov@gmail.com>
2026-07-19 19:58:33 +0500

    refactor: Рефакторинг computed свойства totalTrackedSeconds внутри composable total-progress

--- src/components/ProgressItem.vue
+++ src/components/ProgressItem.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { isActivityItemValid } from '@/validators'
 import { useProgress } from '@/composables/progress'
+import { HUNDRED_PERCENT } from '@/constants'
 
 const props = defineProps({
   activity: {
@@ -17,7 +18,11 @@ const { colorClasses, percent, progressTag } = useProgress(props.activity)
   <li class="flex flex-col gap-1 p-4">
     <div class="truncate text-lg">{{ activity.name }}</div>
     <div class="bg-neutral-200 h-5 overflow-hidden rounded">
-      <div class="h-full" :class="colorClasses" :style="{ width: percent + '%' }"></div>
+      <div
+        class="h-full"
+        :class="['transition-all', colorClasses]"
+        :style="{ width: Math.min(percent, HUNDRED_PERCENT) + '%' }"
+      ></div>
     </div>
     <div class="flex justify-between font-mono">
       <span>{{ percent }}%</span>
