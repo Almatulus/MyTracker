a9f2e87bbc8c9f20122fb502984f9bc06d334ba8
Almatulus <a.kaipzhanov@gmail.com>
2026-07-22 17:25:16 +0500

    feat: Рефакторинг модуля storage

--- src/main.js
+++ src/main.js
@@ -1,29 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './css/style.css'
-import * as storage from './storage.js'
-import { timelineItems } from './timelineItems.js'
-import { activities } from './activities.js'
-import { isToday } from './time.js'
+import { loadState, saveState } from './storage.js'
 
 loadState()
 
 document.addEventListener('visibilitychange', () => {
   if (document.visibilityState === 'hidden') saveState()
 })
-
-function saveState() {
-  storage.save({
-    timelineItems: timelineItems.value,
-    activities: activities.value,
-  })
-}
-
-function loadState() {
-  const state = storage.load()
-
-  timelineItems.value = isToday(new Date(state.date)) ? state.timelineItems : timelineItems.value
-  activities.value = state.activities || activities.value
-}
 
 createApp(App).mount('#app')
